6f17b1d528d727a9b946574b1b573ad48814a3f7
watpnas <watpnas@gmail.com>
2021-08-23 15:15:04 +0700

    Add App2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const app2 = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
 
@@ -8,5 +9,9 @@ app.use(express.static('public'));
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
+app2.post('/echo', function(req, res) {
+    res.json({req: req});
+});
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
+app2.listen(443, () => console.log(`app listening on port ${port}!`));
