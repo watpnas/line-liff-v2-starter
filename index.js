7106a1bf1c68b2b437e31ec1724ab1e1c55d8c2f
watpnas <watpnas@gmail.com>
2021-08-23 16:01:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const app2 = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+const lineKey = process.env.LINE_KEY;
+const telegramKey = process.env.TELEGRAM_KEY;
 
 app.use(express.static('public'));
 
@@ -10,7 +12,7 @@ app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
 });
 app.post('/echo', function(req, res) {
-    res.json({req: req, key: process.env.Key||"999"});
+    res.json({req: req, line: lineKey||"999",telegram:telegramKey});
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
