45532a454478e2684cd863669cbb4b3e636ac3f7
watpnas <watpnas@gmail.com>
2021-08-30 19:06:41 +0700

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -15,6 +15,7 @@ window.onload = function() {
             })
             .then(function(jsonResponse) {
                 myLiffId = jsonResponse.id;
+		console.log(myLiffId);
                 initializeLiffOrDie(myLiffId);
             })
             .catch(function(error) {
